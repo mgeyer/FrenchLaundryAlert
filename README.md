350188c9b1dca182133bf0f6886964e8bd34ba29
Markus Geyer <mgeyer.gbit@gmail.com>
2021-03-26 14:22:19 -0700

    update readme

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# FrenchLaundryAlert
+# Cron Alerts
 
 ## How To Use
 - Create an AWS SNS Topic with SMS subscription
-- Add the script to a new EC2Host
-- Edit Crontab to run the script however often you want.
+
+- Add the script to a new EC2Host (Just copy and paste, and update target-arn)
+
+- Edit Crontab to run the script however often you want. Example: `*/1 * * * * /bin/bash /home/ec2-user/bin/vaccine-alert.sh > /tmp/vaccine-outpout`
--- placer-vaccine-alert.sh
+++ placer-vaccine-alert.sh
@@ -7,8 +7,7 @@ APRIL_FIRST='April 1'
 FILE=/tmp/vaccine-alert-fired
 if [[ "$FL_OUTPUT" == *"$FIFTY_PLUS"* || "$FL_OUTPUT" == *"$APRIL_FIRST"* ]]; then
   if ! test -f "$FILE"; then
-    echo "It's not there."
     touch $FILE
-    aws sns publish --region "us-east-1" --topic-arn "" --message "Vaccine Available for 50+. Go to https://www.placer.ca.gov/vaccineclinics#grounds"
+    aws sns publish --region "us-east-1" --target-arn "" --message "Vaccine Available for 50+. Go to https://www.placer.ca.gov/vaccineclinics#grounds"
   fi
 fi
